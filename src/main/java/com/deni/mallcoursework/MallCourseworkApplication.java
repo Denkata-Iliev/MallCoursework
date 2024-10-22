466619100ed3014be0d096cbf5ea82d6875b9d9a
Denkata-Iliev <denislav9711@gmail.com>
2024-10-22 14:32:21 +0300

    Initial commit

---
+++ src/main/java/com/deni/mallcoursework/MallCourseworkApplication.java
@@ -0,0 +1,13 @@
+package com.deni.mallcoursework;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MallCourseworkApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MallCourseworkApplication.class, args);
+    }
+
+}
