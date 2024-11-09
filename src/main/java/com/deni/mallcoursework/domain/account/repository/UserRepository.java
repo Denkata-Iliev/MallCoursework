0d6f607737279d115082e27aa1c67451d29ad480
Denkata-Iliev <denislav9711@gmail.com>
2024-11-09 21:26:35 +0200

    Create db migration for users table, custom UserDetails and temporary UserDetailsService implementation

---
+++ src/main/java/com/deni/mallcoursework/domain/account/repository/UserRepository.java
@@ -0,0 +1,10 @@
+package com.deni.mallcoursework.domain.account.repository;
+
+import com.deni.mallcoursework.domain.account.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, String> {
+    User findByEmail(String email);
+}
